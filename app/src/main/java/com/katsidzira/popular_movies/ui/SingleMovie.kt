e4d740d51567fb48d116b0b1462acc445eb67afa
Tafadzwa Katsidzira <tafadzwakatsidzira@Tafadzwas-MacBook-Pro-2.local>
2020-03-25 23:07:50 -0400

    initial commit

---
+++ app/src/main/java/com/katsidzira/popular_movies/ui/SingleMovie.kt
@@ -0,0 +1,13 @@
+package com.katsidzira.popular_movies.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.katsidzira.popular_movies.R
+
+class SingleMovie : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_single_movie)
+    }
+}
